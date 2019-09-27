71009032ee0ffc586dec9f8b7425e95efc50c891
Max Bender <maxbbender@gmail.com>
2019-09-27 14:57:09 -0400

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # python-png-info
 A Python script to grab some information about a PNG file located at a URL
+
+### Set Up
+
+Install Python/Set up Virtual Environment for Python 3.X and run
+
+```
+pip install -r requirements.txt
+```
+
+### Parameters
+
+There are two environment variables you can set to override defaults 
+
+###### IMAGE_URL
+
+**Default** `https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png`
+
+```
+export IMAGE_URL=https://logos-download.com/wp-content/uploads/2018/07/Marvel_logo_red.png
+```
+
+###### TARGET_FILE
+
+**Default** `header.txt`
+
+```
+export TARGET_FILE=a-different-header.txt
+```
+
+## Usage
+
+Once your variables are set to your liking run `python main.py` to print out the header info to your console as well as generate a text file containing the output
+
+#### Problem Description
+
+> Write a script that will:
+>
+> Display the request header in a terminal window for https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png
+> Save the header information into a text file named header.txt
+> Upload the linked image from step 1, header.txt file, and your script to an S3 bucket
+> Then:
+> 
+> 1.       Provide a link to your S3 bucket so the files can be downloaded.
+>
+> 2.       Provide instructions on how to run your script.
+
---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+from requests.exceptions import HTTPError
+import os
+from io import BytesIO
+from pprint import pprint, pformat
+import re
+
+image_url = os.environ.get('IMAGE_URL') or 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+target_file = os.environ.get('TARGET_FILE') or 'header.txt'
+filename = None
+
+print('Attempting a request against \n   %s' % image_url)
+
+regex_pattern = 'https:\/\/.*\/(.*)\.(jpg|gif|png)'
+regex_search = re.match(regex_pattern, image_url)
+if regex_search:
+    filename = '%s.%s' % (regex_search.group(1), regex_search.group(2))
+    print('URL is Valid')
+else:
+    print('URL is invalid... exiting')
+    exit(1)
+
+try:
+    response = requests.get(image_url)
+    response.raise_for_status()
+except HTTPError as err:
+    print('HTTPError : %s' % err)
+except Exception as err:
+    print('Misc Exception : %s' % err)
+else:
+    print('Request Successfull for %s' % filename)
+    
+    # Print Headers to Console
+    print('Headers for \n   %s\n' % image_url)
+    json_string = json.dumps(response.headers.__dict__['_store'])
+    headers_json = json.loads(json_string)
+    pprint(headers_json)
+
+    # Save Header content to header.txt
+    print('Writing to %s' % target_file)
+    f = open(target_file,"w+")
+    f.write(pformat(headers_json))
+    f.close()
+    print('Finished Writing %s' % target_file)
